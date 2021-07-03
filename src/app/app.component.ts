8251aeab6f7d48016970c9a620ba5252bd5da22f
AnsonJahaziel <57742199+AnsonJahaziel@users.noreply.github.com>
2021-07-02 22:52:35 -0600

    guacamole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'componentsChallenge';
+
+  title = 'guacamole';
+  isShow = true;
+  isShow2 = true;
+  isShow3 = true;
+  isShow4 = true;
+  isShow5 = true;
+  isShow6 = true;
+
+  toggleDisplay() {
+    this.isShow = !this.isShow;
+    this.isShow2 = !this.isShow2;
+    this.isShow3 = !this.isShow3;
+    this.isShow4 = !this.isShow4;
+    this.isShow5 = !this.isShow5;
+    this.isShow6 = !this.isShow6;
+  }
+
+  toggleDisplay1() {
+    this.isShow = !this.isShow;
+  }
+  toggleDisplay2() {
+    this.isShow2 = !this.isShow2;
+  }
+  toggleDisplay3() {
+    this.isShow3 = !this.isShow3;
+  }
+  toggleDisplay4() {
+    this.isShow4 = !this.isShow4;
+  }
+  toggleDisplay5() {
+    this.isShow5 = !this.isShow5;
+  }
+  toggleDisplay6() {
+    this.isShow6 = !this.isShow6;
+  }
 }
---
+++ src/app/guacamole/guacamole.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guacamole',
+  templateUrl: './guacamole.component.html',
+  styleUrls: ['./guacamole.component.css']
+})
+export class GuacamoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
